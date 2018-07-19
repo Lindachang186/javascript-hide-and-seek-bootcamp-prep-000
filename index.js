a0bc9bcb350b12ea7bf48dd71723fd52a42bfc6a
Linda Chang <lindachang186@gmail.com>
2018-07-19 14:35:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,4 +13,17 @@ function increaseRankBy(n){
  }
 }
 
-function deepestChild()
+function deepestChild() {
+  let find = document.getElementById('grand-node');
+  let deep = document.getElementById('grand-node').children
+  while(deep) {
+    if(find(deep) {
+      return find
+    } if (Array.isArray(deep)) {
+      for(let i = 0; i < current.length; i++) {
+        return deep[i].innerHTML
+      }
+    }
+
+  }
+}
